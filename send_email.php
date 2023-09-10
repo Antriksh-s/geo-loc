4a9f0e76f7c78b8b88f923ee508b153e722f1fd3
Antriksh-s <92921158+Antriksh-s@users.noreply.github.com>
2023-09-10 12:01:38 +0530

    update

---
+++ send_email.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $latitude = $_POST["latitude"];
+    $longitude = $_POST["longitude"];
+
+    // Define your email parameters here (e.g., recipient, subject, message)
+    $to = "recipient@example.com";
+    $subject = "Geolocation Data";
+    $message = "Latitude: $latitude\r\nLongitude: $longitude";
+
+    // Additional headers
+    $headers = "From: your_email@example.com";
+
+    // Send the email
+    mail($to, $subject, $message, $headers);
+}
+?>
